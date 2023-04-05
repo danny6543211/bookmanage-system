7cddab8681debd9d3873478364071d699e9d4080
danny6543211 <danny6543211@gmail.com>
2023-04-05 21:52:20 +0800

    test

--- include/client.h
+++ include/client.h
@@ -11,9 +11,13 @@ class client
 public:
     client();
     ~client();
-    void client_connect();
+    // 设定好信息
     void set_message(int type, int action, char *account, char *password, char *book_name);
+    // 与服务器连接
+    void client_connect();
+    // 传递信息到服务器
     int send_message();
+    // 取得服务器回传的值
     int get_return_value();
     void test();
 private:
