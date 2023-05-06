9bc44c6f8830638e809ca5a12b6346ee0fb53d98
danny6543211 <danny6543211@gmail.com>
2023-05-06 19:01:17 +0800

    .\test.cpp

--- test.cpp
+++ test.cpp
@@ -2,19 +2,13 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
-
-std::vector<std::string> foo(std::string str);
+#include <cmath>
 
 int main()
 {
-    std::string buffer = "test_book1\nSat May 06 14:59:47 2023 \nMon Jun 05 14:59:47 2023\n";
 
-    std::vector<std::string> res = foo(buffer);
 
-    for (std::vector<std::string>::iterator it = res.begin(); it != res.end(); it++)
-    {
-        std::cout << *it;
-    }
+
 
 }
 
