8caebd0f62872be1944db0eb87826f0d9333fd40
danny6543211 <danny6543211@gmail.com>
2023-05-17 19:06:39 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,40 @@ int main()
     server server;
     user user;
     manager manager;
-    
+
     while (1)
     {
         server.server_accept();
         server.receive_message();
-        // 查看是管理員還是普通用戶
+        // 註冊帳號
+        int return_value = -2;
         int type = server.get_type();
+
+        // 沒有此帳戶查看是否要註冊
+        if (type == -1)
+        {
+            if (server.get_action() == SIGN_UP)
+            {
+                user.init(server.get_account(), server.get_password(), server.get_book_name());
+                return_value = user.sign_up();
+                if (return_value == 1)
+                    server.set_return_buffer("sign up successful");
+                else if (return_value == 0)
+                    server.set_return_buffer("account exist already");
+                std::cout << "type:visitor" << std::endl;
+            }
+            else 
+            {
+                std::cout << "type:account not exist" << std::endl;
+            }
+        }
+
+        // 查看是管理員還是普通用戶
         if (type == USER)
             std::cout << "type:user" << std::endl;
         else if (type == MANAGER)
             std::cout << "type:manager" << std::endl;
-        else 
-            std::cout << "type:account not exist" << std::endl;
-        int return_value = -2;
+
         if (type == USER)
         {
             user.init(server.get_account(), server.get_password(), server.get_book_name());
@@ -43,7 +63,7 @@ int main()
                 break;
             case RENT_BOOK:
                 return_value = user.rent_book();
-                    server.set_return_value(return_value);
+                server.set_return_value(return_value);
                 if (return_value == 1)
                     server.set_return_buffer("rent book successful");
                 else if (return_value == 0)
@@ -106,13 +126,10 @@ int main()
                 break;
             }
         }
-        else 
-        {
-            server.set_return_value(-1);
-            server.set_return_buffer("account not exist");
-        }
+
         std::cout << "return value:" << server.get_return_value() << std::endl;
-        std::cout << "return buffer:\n" << server.get_return_buffer() << std::endl;
+        std::cout << "return buffer:\n"
+                  << server.get_return_buffer() << std::endl;
         server.send_result();
         server.server_close();
 
